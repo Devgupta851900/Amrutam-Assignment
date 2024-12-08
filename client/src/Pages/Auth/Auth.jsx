3589f3580b95bde7a34088bb2e46bd8c94ec9895
DevGupta <devgupta851900@gmail.com>
2024-12-08 21:04:21 +0530

    made build

--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import authImage from "../assets/authImage.jpeg";
+import authImage from "../../assets/authImage.jpeg";
 import { toast } from "react-hot-toast";
 import { login, signup } from "../../utils/api.js";
 
